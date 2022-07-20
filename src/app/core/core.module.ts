685f0add01bdcd1e2a310ee85ef794d9617d7caf
adelinaanescu <84186552+adelinaanescu@users.noreply.github.com>
2022-07-20 13:38:56 +0300

    Added Dashbord and Nav components
    ! The components work only if we add them in app-component.html, does not work in home component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,28 +2,14 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { PollComponentComponent } from "./poll-component/poll-component.component";
 import { HomeComponentComponent } from "./home-component/home-component.component";
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { NavbarComponentComponent } from './home-component/navbar-component/navbar-component.component';
 
 @NgModule({
     declarations: [
         PollComponentComponent,
-        HomeComponentComponent,
-        NavbarComponentComponent,
+        HomeComponentComponent
     ],
     imports: [
-        CommonModule,
-        LayoutModule,
-        MatToolbarModule,
-        MatButtonModule,
-        MatSidenavModule,
-        MatIconModule,
-        MatListModule
+        CommonModule
 ]
 
 })
